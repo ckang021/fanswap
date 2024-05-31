b1e9c08865a930cb2ccad5a01c22d588de246657
Chris Kang <chriskangfamily@gmail.com>
2024-05-30 20:54:09 -0700

    search and all products done

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,10 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+import { productSearch } from "../../redux/products";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import { FaShoppingCart } from "react-icons/fa";
 import navLogo from "./navlogoWhite.png"
 
+
 function Navigation() {
+  const [search, setSearch] = useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const submitSearch = async (e) => {
+    e.preventDefault()
+    await dispatch(productSearch(search))
+    const query = search.split(' ').join('+')
+    navigate(`/products/?name=${query}`)
+    setSearch('')
+  }
+
   const cartButton = (e) => {
     e.preventDefault()
     alert("Feature coming soon...")
@@ -18,6 +38,18 @@ function Navigation() {
           </NavLink>
         </li>
 
+        <li>
+          <form onSubmit={submitSearch}>
+            <input
+              type="text"
+              placeholder="Search for products"
+              value={search}
+              onChange={handleSearch}
+            />
+            <button>Search</button>
+          </form>
+        </li>
+
         <li id="profile-cart-button">
           <button onClick={cartButton}>
             <FaShoppingCart />
@@ -25,7 +57,7 @@ function Navigation() {
           <ProfileButton />
         </li>
       </div>
-    </ul>
+    </ul >
   );
 }
 
--- react-vite/src/components/Products/Products.jsx
+++ react-vite/src/components/Products/Products.jsx
@@ -1,9 +1,56 @@
+import { useEffect } from "react";
+import { everyProducts, productSearch } from "../../redux/products";
+import { useDispatch, useSelector } from "react-redux";
+import { useSearchParams, NavLink } from "react-router-dom";
+import "./Products.css";
+
 function Products() {
+  const dispatch = useDispatch();
+
+  const allProducts = useSelector(state => state.products.allProducts.products) || [];
+  const searchResults = useSelector(state => state.products.search) || [];
+
+  const [query] = useSearchParams();
+  const querySearch = query.get('name')?.split(' ').join('+');
+
+  useEffect(() => {
+    if (querySearch) {
+      dispatch(productSearch(querySearch));
+    } else {
+      dispatch(everyProducts());
+    }
+  }, [dispatch, querySearch]);
+
+  useEffect(() => {
+    if (!querySearch && searchResults.length > 0) {
+      dispatch(everyProducts());
+    }
+  }, [dispatch, querySearch, searchResults.length]);
+
+  const productList = querySearch && searchResults.length > 0 ? searchResults : allProducts;
+
   return (
-    <div>
-      All Products
+    <div className="all-products-wrapper">
+      <h2>All Products</h2>
+      <div className="products-layout-container">
+        {productList.length === 0 ? (
+          <p>No products found</p>
+        ) : (
+          productList.map(product => (
+            <div key={product.id} className="product-card">
+              <NavLink to={`/products/${product.id}`}>
+                <img src={product.preview_image} alt={product.name} />
+                <div>
+                  <p>{product.name}</p>
+                  <p>{product.price}</p>
+                </div>
+              </NavLink>
+            </div>
+          ))
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
